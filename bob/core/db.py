9508ca16880fd58649c25c65ea7e771458fe1fa9
erseyang <erseyang@gmail.com>
2017-10-24 23:07:02 -0500

    Update db.py
    更新一下数据库信息

--- bob/core/db.py
+++ bob/core/db.py
@@ -13,8 +13,8 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-sqla_engine = create_engine('mysql://bob:bobspider@114.55.151.132:3306/bob_spider?charset=utf8',convert_unicode=True, encoding='utf-8', echo=False, pool_size = 100)
-# sqla_engine = create_engine('mysql://root:CP2016_@new@120.132.51.138:3506/bob?charset=utf8',convert_unicode=True, encoding='utf-8', echo=False, pool_size = 100)
+sqla_engine = create_engine('mysql://localhost/bob_spider?charset=utf8',convert_unicode=True, encoding='utf-8', echo=False, pool_size = 100)
+# sqla_engine = create_engine('mysql://localhost?charset=utf8',convert_unicode=True, encoding='utf-8', echo=False, pool_size = 100)
 ##redis连接池
 Base = declarative_base()
 
